5b9e3d9e19a09c59ce626a18f1301dc1ef72b376
eleves <patriciashimizu@hotmail.com>
2017-07-18 15:57:16 -0400

    layout2

--- patriciaToDoList/ViewController.swift
+++ patriciaToDoList/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource  {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +20,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    //---------------------
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        tableView.backgroundColor = UIColor.clear
+        return 20
+    }
+    //---------------------
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = UITableViewCell(style:UITableViewCellStyle.default, reuseIdentifier:"proto")
+        cell.textLabel!.text = "Hello World"
+        cell.textLabel?.textColor = UIColor.black
+        cell.backgroundColor = UIColor.clear
+        return cell
+    }
+    //---------------------
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedCell:UITableViewCell = tableView.cellForRow(at: indexPath as IndexPath)!
+        selectedCell.contentView.backgroundColor = UIColor.darkGray
+    }
+    //---------------------
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCellEditingStyle.delete {
+            tableView.deleteRows(at: [indexPath as IndexPath], with: UITableViewRowAnimation.automatic)
+        }
+    }
+    //---------------------
 }
 
